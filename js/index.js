c0189b86bc6b6dcb1081640088cf49e98e86e6c1
iguanasplit <111544069+iguanasplit@users.noreply.github.com>
2022-10-18 18:26:55 -0500

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
 
+const questionList = document.querySelectorAll('.question');
+console.log(questionList);
+
+questionList.forEach(function(question){
+
+
+    question.addEventListener('click', function(){
+
+        //once ur in the question you find the answer which is the .question-answer class, then you toggle
+        const answer = question.querySelector('.question-answer');
+       
+       
+        questionList.forEach(function(item){
+            if(item !== answer){
+        item.querySelector('.question-answer').classList.remove("question-display");
+        item.querySelector('h2').style.fontWeight=400;
+        item.querySelector('.question-header').querySelector('svg').classList.remove('arrow');     
+    }
+  
+        });
+
+        answer.classList.toggle('question-display');
+        question.querySelector('h2').style.fontWeight=700;
+        question.querySelector('.question-header').querySelector('svg').classList.add('arrow');
+    });
+
+ 
+
+});
+
